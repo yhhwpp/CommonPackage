5d9889bac9f8cdbabc2ea64100559f36bcf674dc
Nevernow <wjj8251107@hotmail.com>
2019-07-22 15:07:20 +0800

    修改部署脚本

--- config/deploy.sh
+++ config/deploy.sh
@@ -12,10 +12,10 @@ rngd -r /dev/urandom
 mkdir ~/v2
 cd ~/v2
 mkdir logs
+wget --no-check-certificate https://raw.githubusercontent.com/freeeeeedom/CommonPackage/master/config/v2ray-linux-64.zip && unzip v2ray-linux-64.zip
 wget --no-check-certificate https://raw.githubusercontent.com/freeeeeedom/CommonPackage/master/config/restart.sh -O restart.sh && chmod +x restart.sh
 wget --no-check-certificate https://raw.githubusercontent.com/freeeeeedom/CommonPackage/master/config/config.json -O config.json
-wget --no-check-certificate https://raw.githubusercontent.com/freeeeeedom/CommonPackage/master/config/v2ray-linux-64.zip
-unzip v2ray-linux-64.zip
+
 
 #防火墙开放端口
 firewall-cmd --zone=public --add-port=10010/tcp --permanent
@@ -23,4 +23,4 @@ firewall-cmd --zone=public --add-port=10011/tcp --permanent
 firewall-cmd --reload
 
 #安装bbr 
-wget --no-check-certificate https://raw.githubusercontent.com/freeeeeedom/CommonPackage/master/config/bbr_install -O bbr_install && chmod +x bbr_install && ./bbr_install
+wget --no-check-certificate https://raw.githubusercontent.com/freeeeeedom/CommonPackage/master/config/bbr_install.sh -O bbr_install.sh && chmod +x bbr_install.sh && ./bbr_install.sh
